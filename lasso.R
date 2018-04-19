cb2b9584e5e2dc1bac858282fb03cb34b3ddb0a9
Guilherme Avelar <gui.coelhonetto@gmail.com>
2018-04-19 15:00:46 -0300

    Mudanças no organize_merge e adicionar lasso.R

---
+++ lasso.R
@@ -0,0 +1,51 @@
+library(glmnet)
+
+# No scientif notation
+options(scipen=999)
+
+# Main paths
+main.path="/Users/guicoelhonetto/Documents/GitHub/r-scripts/"
+data.path=paste0(main.path,"data/")
+fig.path=paste0(main.path,"fig/")
+
+# Read data
+
+load(paste0(data.path,"dataset.RData"))
+dataset.lasso=dataset[complete.cases(dataset[,c("perc_freq_lp",lasso.vars)]),]
+
+# Residualize data
+
+resid.fe=function(x){
+  reg=felm(x ~ 0 | cod_turma)
+  resid=reg$resid
+  return(resid) 
+}
+
+# Lasso
+
+x=as.matrix(dataset.lasso[,lasso.vars])
+y=as.matrix(dataset.lasso$perc_freq_lp)
+
+# Lasso with cross-validation
+
+lambda.grid=exp(seq(-12,0,length.out=100))
+cvlasso=cv.glmnet(x,y,nfolds=5,lambda = lambda.grid)
+
+# Save plot
+png(filename=paste0(fig.path,"cvlasso.png"))
+plot(cvlasso)
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- merge_organize.R
+++ merge_organize.R
@@ -1,21 +1,47 @@
 library(readstata13)
 library(readxl)
-library(xlsx)
 library(dplyr)
+
+# No scientif notation
+options(scipen=999)
 
 # Main paths
 main.path="/Users/guicoelhonetto/Documents/GitHub/r-scripts/"
 data.path=paste0(main.path,"data/")
 
-# Read data
+### Read data
+
+# Read outcomes data
 dataset1=read.dta13(paste0(data.path,"diff_ago8.dta"))
 
+# Read characteristics data
 dataset2=read.dta13(paste0(data.path,"sms_escola_mar21.dta"))
-select.vars=c("menina","idade","parda","preta","bolteim_mat1","mae","idade_resp","parda_resp","preta_resp","educ_baixa","educ_EF","educ_EM","renda_1SM","renda_1a3SM")
+
+# Rename boeltim to boletim
+names(dataset1)=gsub("bolteim","boletim",names(dataset1))
+names(dataset2)=gsub("bolteim","boletim",names(dataset2))
+
+# Select variables
+select.vars=c("menina","idade","parda","preta","mae","idade_resp","parda_resp","preta_resp","educ_baixa","educ_EF","educ_EM","renda_1SM","renda_1a3SM")
 
 dataset2=dataset2[,c("ra",select.vars)]
 
+# Read sms data
 db.sms=read_excel(paste0(data.path,"interacoes_smsescola.xlsx"))
+
+# Change "Resposta extra" to preceeding question
+db.sms$extra_ans=ifelse(db.sms$question=="Resposta extra",1,0)
+db.sms[which(db.sms$question=="Resposta extra"),]$question=db.sms[which(db.sms$question=="Resposta extra")-1,]$question
+
+## Drop unnecessary data
+
+dataset1=dataset1[,!grepl("t_eduq",names(dataset1))]
+for(i in 1:4){dataset1[,paste0("boletim_lp",i)]=ifelse(dataset1$t!=i,NA,dataset1[,paste0("boletim_lp",i)])}
+
+dataset1$boletim_lp=rowSums(dataset1[,paste0("boletim_lp",1:4)],na.rm=T)
+dataset1=dataset1[,!names(dataset1)%in%paste0("boletim_lp",1:4)]
+dataset1=dataset1[,!names(dataset1)%in%paste0("eduq_puro",1:3)]
+dataset1=dataset1[,!names(dataset1)%in%c("s","boletim_mat_z","boletim_lp_z","t2")]
 
 ## Find phone-ra correspondence through another database
 
@@ -31,9 +57,16 @@ dataset1=merge(dataset1,dataset2,by="ra",all.x=T)
 
 # Drop obs
 
+# eduq_freq - Tratamento Engajamento - Frequencia (frequency)
+# eduq_time - Tratamento Engajamento - Hora do dia (time)
+# eduq_feed - Tratamento Engajamento - Feedback (interactivity)
+
 dataset1=dataset1[dataset1$t%in%c(3,4),]
+dataset1=dataset1[dataset1$eduq_feed==1,]
 
 ### Create sms variables
+
+ncol.oldvars=ncol(db.sms)
 
 # Number of characters
 db.sms$nchar=nchar(db.sms$answer)
@@ -42,15 +75,36 @@ db.sms$nchar=nchar(db.sms$answer)
 #db.sms$punct=grepl("[[:punct:]]",db.sms$answer)
 
 # Lower case
-db.sms$punct=ifelse(!grepl("[[:lower:]]",db.sms$answer)==T,1,0)
+db.sms$lower=ifelse(!grepl("[[:lower:]]",db.sms$answer)==T,1,0)
 
 # Presence of "SIM"
 db.sms$ans.yes=ifelse(grepl("sim",db.sms$answer)==T,1,0)
 
+# 
+created.vars=names(db.sms)[(ncol.oldvars+1):ncol(db.sms)]
 
+### Collapse sms dataset
 
+# Create bimester variable
+db.sms$t=ifelse(db.sms$semana%in%1:9,3,NA)
+db.sms$t=ifelse(db.sms$semana%in%10:18,3,db.sms$t)
 
+# Change name
+names(db.sms)[names(db.sms)=="semana"]="week"
 
+# Collapse
+dataset.sms=db.sms %>% group_by(phone,t) %>% summarise_at(c("week",created.vars),funs(mean,max,sum,min))
+
+# Merge dataset1 and db.sms
+
+dataset1=merge(dataset1,dataset.sms,by=c("phone","t"),all.x=T)
+
+# Rename dataset1
+dataset=dataset1
+
+lasso.vars=grep(paste0(created.vars,collapse="|"),names(dataset),value=T)
+
+save(dataset,lasso.vars,file=paste0(data.path,"dataset.RData"))
 
 
 

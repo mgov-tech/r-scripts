9c1f5062a2a88dc0ecc80bbc0c12d28bfbef3bff
Guilherme Avelar <gui.coelhonetto@gmail.com>
2018-05-22 18:41:25 -0300

    Mudanças

--- causal_trees.R
+++ causal_trees.R
@@ -83,71 +83,85 @@ find.groups=function(branch){
   return(groups)
 }
 
-
 # Trees
 
+main_effect=NA
+main_effect_fit=NA
 groups=list()
+
 for(k in c("","_fit")){
-
-fmla=paste0(chosen.outcome,k," ~ parda + parda_resp + preta + preta_resp + mae + renda_1SM + renda_1a3SM + educ_EM + educ_baixa + menina + idade_resp")
+  
+  fmla=paste0(chosen.outcome,k," ~ parda + parda_resp + preta + preta_resp + mae + renda_1SM + renda_1a3SM + educ_EM + educ_baixa + menina + idade_resp")
   
   
-for(i in treat.fit){
-  
-  dt.temp=dataset[!is.na(dataset[,i]),]
-  
-  tree = causalTree(fmla, 
-                    data = dt.temp, treatment = dt.temp[,i],
-                    split.Rule = "TOT", 
-                    cv.option = "fit", 
-                    minsize = 30,  
-                    cv.Honest = T, 
-                    split.Bucket = T, 
-                    xval = 10, 
-                    propensity = 0.8)
-  
-  opcp = tree$cptable[, 1][which.min(tree$cptable[,4])]
-  opfit = prune(tree, cp = opcp)
-  
-  png(filename=paste0(fig.path,i,k,".png"),width=480,height=480)
-  rpart.plot(opfit,           
-             type = 1, 
-             extra=101, 
-             under=T, 
-             digits=2, 
-             box.palette=0)
-  dev.off()
-  
-  branches=find.branches(opfit)
-  
-  tree.groups=list()
-  for(j in seq_along(branches)){
-  tree.groups[[j]]=find.groups(branches[[j]])
+  for(i in treat.fit){
+    
+    dt.temp=dataset[!is.na(dataset[,i]),]
+    
+    tree = causalTree(fmla, 
+                      data = dt.temp, treatment = dt.temp[,i],
+                      split.Rule = "TOT", 
+                      cv.option = "fit", 
+                      minsize = 30,  
+                      cv.Honest = T, 
+                      split.Bucket = T, 
+                      xval = 10, 
+                      propensity = 0.8)
+    
+    opcp = tree$cptable[, 1][which.min(tree$cptable[,4])]
+    opfit = prune(tree, cp = opcp)
+    
+    png(filename=paste0(fig.path,i,k,".png"),width=480,height=480)
+    rpart.plot(opfit,           
+               type = 1, 
+               extra=101, 
+               under=T, 
+               digits=2, 
+               box.palette=0)
+    dev.off()
+    
+    branches=find.branches(opfit)
+    
+    tree.groups=list()
+    for(j in seq_along(branches)){
+      tree.groups[[j]]=find.groups(branches[[j]])
+    }
+    groups[[i]]=tree.groups
+    
   }
-  groups[[i]]=tree.groups
+  
+  for(i in treat.fit){
+    
+    dataset[,paste0(i,"_group_effect",k)]=NA
+    dataset[,paste0(i,"_main_effect",k)]=NA
+    
+    if(k!="_fit"){
+      main_effect[i]=groups[[i]][[1]]$main.effect
+
+    } else {
+      main_effect_fit[i]=groups[[i]][[1]]$main.effect
+
+    }
+    
+  }
+  
+  for(i in seq_along(groups)){
+    
+    for(j in seq_along(groups[[i]])){
+      if(!is.null(groups[[i]][[j]]$conds)){
+        eval(parse(text=paste0("dataset$",names(groups[i]),"_group_effect",k,"=","ifelse(",paste0("dataset$",groups[[i]][[j]]$conds,collapse="&"),",",groups[[i]][[j]]$group.effect,",","dataset$",names(groups[i]),"_group_effect",k,")")))
+      }
+    }
+    eval(parse(text=paste0("dataset$",names(groups)[i],"_main_effect",k,"=",groups[[i]][[j]]$main.effect)))
+    
+  }
   
 }
 
-for(i in treat.fit){
+main_effect=main_effect[!is.na(main_effect)]
+main_effect_fit=main_effect_fit[!is.na(main_effect_fit)]
 
-  dataset[,paste0(i,"_group_effect",k)]=NA
-  dataset[,paste0(i,"_main_effect",k)]=NA
-
-}
-
-for(i in seq_along(groups)){
-  
-  for(j in seq_along(groups[[i]])){
-  if(!is.null(groups[[i]][[j]]$conds)){
-  eval(parse(text=paste0("dataset$",names(groups[i]),"_group_effect",k,"=","ifelse(",paste0("dataset$",groups[[i]][[j]]$conds,collapse="&"),",",groups[[i]][[j]]$group.effect,",","dataset$",names(groups[i]),"_group_effect",k,")")))
-  }
-  }
-  eval(parse(text=paste0("dataset$",names(groups)[i],"_main_effect",k,"=",groups[[i]][[j]]$main.effect)))
-   
-} 
-}
-
-save(dataset,file=paste0(data.path,"data_final.RData"))
+save(dataset,main_effect,main_effect_fit,groups,file=paste0(data.path,"data_final.RData"))
 
 
 
--- main_stats.R
+++ main_stats.R
@@ -1,6 +1,8 @@
 library(devtools)
 library(causalTree)
 library(stringr)
+library(foreach)
+library(doParallel)
 
 # Source personal configurations
 source('config.R')
@@ -10,29 +12,71 @@ fig.path=paste0(main.path,"fig/")
 
 load(paste0(data.path,"data_final.RData"))
 
+# Outcomes
+outcomes = c("boletim_mat","perc_freq_mat","boletim_lp","perc_freq_lp")
+
+chosen.outcome=outcomes[1]
+
+# Characteristics vector
+chars=c("parda","parda_resp","preta","preta_resp","mae","renda_1SM","renda_1a3SM","educ_EM","educ_baixa","menina","idade_resp")
+
 # Choose variations of treatments
 treat=paste0("v",1:24)
 treat.fit=treat[sapply(1:24,function(x) sum(dataset[!dataset$eduq_feed%in%c(2,9),paste0("v",x)]==1,na.rm=T))>0]
 
 # Create variables
 
-dataset$R_real=NA
+dataset$R_oracle=NA
 dataset$R_proxy=NA
 
 for(i in treat.fit){
-   dataset$R_real=ifelse(dataset[,i]==1,dataset[,paste0(i,"_main_effect")],dataset$R_real)
-   dataset$R_proxy=ifelse(dataset[,i]==1,dataset[,paste0(i,"_main_effect_fit")],dataset$R_proxy)
+  dataset$R_oracle =ifelse(dataset[,i]==1 & !is.na(dataset[,i]),dataset[,paste0(i,"_main_effect")],dataset$R_oracle)
+  dataset$R_proxy=ifelse(dataset[,i]==1 & !is.na(dataset[,i]),dataset[,paste0(i,"_main_effect_fit")],dataset$R_proxy)
 }
 
-dataset$R_real_max=apply(dataset[,grep("_fit",grep("group_effect",names(dataset),fixed=T,value=T),invert=T,fixed=T,value=T)],1,max)
+dataset$R_oracle_max=apply(dataset[,grep("_fit",grep("group_effect",names(dataset),fixed=T,value=T),invert=T,fixed=T,value=T)],1,max)
 dataset$R_proxy_max=apply(dataset[,grep("group_effect_fit",names(dataset),fixed=T,value=T)],1,max,na.rm=T)
 
 # Compute statistics
 
-Delta_real=mean(dataset$R_real_max-dataset$R_real,na.rm=T)
+Delta_oracle=mean(dataset$R_oracle_max-dataset$R_oracle,na.rm=T)
 Delta_proxy=mean(dataset$R_proxy_max-dataset$R_proxy,na.rm=T)
 
-quality=Delta_proxy/Delta_real
+proxy.quality=Delta_proxy/Delta_oracle
+
+# Predicted impact
+
+ols=felm(as.formula(paste0(chosen.outcome,"~",paste0(feat.select[-1],collapse="+"))),data=dataset)
+
+dataset$postlasso=NA
+dataset[dataset$eduq_feed!=9, ]$postlasso=ols$fitted.values
+
+predicted_impact=mean(dataset$postlasso,na.rm=T)-mean(dataset[dataset$treat_cod==25,chosen.outcome])
+
+# Re-assingment probabilities
+
+prob=main_effect_fit/sum(main_effect_fit)
+
+### Simulation
+
+cores=detectCores()
+cl <- makeCluster(cores[1]-1)
+registerDoParallel(cl)
+
+weeks=8
+
+DELTA_AI <- foreach(i=1:1000, .combine=rbind) %dopar% {
+  
+  rea=matrix(sample(names(prob),size=nrow(dataset)*weeks,replace=T,prob=prob),nrow(dataset),weeks)
+  
+  R_simul_avg=sapply(1:nrow(dataset), function(x) mean(dataset[,paste0(rea[x,],"_group_effect")],na.rm=T))
+  
+  delta=mean(R_simul_avg-dataset$R_oracle,na.rm=T)
+  
+  delta
+}
+
+stopCluster(cl)
 
 print(quality)
 
